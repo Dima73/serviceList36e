8a6f45936e12e59eb71caf8038634e04c74c10fe
Dimitrij <Dima-73@inbox.lv>
2022-10-23 16:36:35 +0300

    update serviceList

--- serviceList.py
+++ serviceList.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-version = "0.3"
+version = "0.4"
 
-serviceIptvUrl = [("4097:0:1:3:1F:FD:1680000:0:0:0:rtmp%3a//tvsoyuz.cdnvideo.ru/tvsoyuz/soyuz:Союз (ТВ)", "soyuz36"), ("4097:0:1:29E3:3CF0:13E:820000:0:0:0:rtmp%3a//tvsoyuz.cdnvideo.ru/tvsoyuz/soyuz:Союз (ТВ)", "soyuz13"), \
-	("4097:0:1:3EF:A:70:1680000:0:0:0:rtmp%3a//spastv.cdnvideo.ru/spastv-live/spastv.sdp:Спас (TB)", "spas"), ("4097:0:2:4:1F:FD:1680000:0:0:0:http%3a//217.24.181.164%3a8000/stream.ogg:Воскресение (Радио)", "voskresenie"), ("4097:0:2:3AAC:F:FD:1680000:0:0:0:http%3a//icecast.radonezh.cdnvideo.ru%3a8000/rad128:Радонеж (Радио)", "radonezh")]
+serviceIptvUrl = [("4097:0:1:3:1F:FD:1680000:0:0:0:rtmp%3a//tvsoyuz.cdnvideo.ru/tvsoyuz/soyuz:Союз (ТВ)", "soyuz36"), \
+	("4097:0:2:4:1F:FD:1680000:0:0:0:http%3a//217.24.181.164%3a8000/stream.ogg:Воскресение (Радио)", "voskresenie"), ("4097:0:2:3AAC:F:FD:1680000:0:0:0:http%3a//icecast.radonezh.cdnvideo.ru%3a8000/rad128:Радонеж (Радио)", "radonezh")]
 
 refresh36listservices = [('1:0:1:3EF:A:70:1680000:0:0:0:'), ('1:0:1:3:1F:FD:1680000:0:0:0:'), ('1:0:2:3AAC:F:FD:1680000:0:0:0:')]
